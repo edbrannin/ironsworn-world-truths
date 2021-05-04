d1203c6941bc44a058b26c168f4852df1855d07c
Ed Brannin <edbrannin@gmail.com>
2021-05-03 22:06:31 -0400

    Add drop-shadows, center things, print styles

--- src/TruthCategory.js
+++ src/TruthCategory.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 const Choices = styled.ol`
@@ -19,57 +20,110 @@ const SelectButton = styled.button`
   width: 100%;
   padding: 1em;
   border-radius: 1em;
+  @media print {
+    display: none;
+  }
+`;
+
+const CategoryName = styled.h2`
+  text-align: center;
 `;
 
 const Description = styled.p`
   white-space: pre-wrap;
 `;
 
-const QuestStarter = styled.p`
+const QuestStarterStyle = styled.p`
   white-space: pre-wrap;
   font-style: italic;
   ::before {
-    content: "Quest Starter:";
+    content: "Quest Starter: ";
   };
 `;
+
+const HiddenQuestStarter = styled.p`
+  font-style: italic;
+  @media print {
+    display: none;
+  }
+`;
+
+const QuestStarter = ({ children }) => {
+  const [ show, setShow ] = useState(true);
+  if (show) {
+    return (
+      <QuestStarterStyle onClick={() => setShow(false)}>{children}</QuestStarterStyle>
+    )
+  }
+  return (
+    <HiddenQuestStarter onClick={() => setShow(true)}>Show quest starter</HiddenQuestStarter>
+  )
+}
 
 const Wrapper = styled.div`
   padding: 1em;
   border-top: 1px solid gray;
+  @media print {
+    page-break-inside: avoid;
+  }
+`;
+
+const ResetButton = styled.button`
+  display: block;
+  padding: 0.5em;
+  margin-left: auto;
+  margin-right: auto;
+  @media print {
+    display: none;
+  }
+`;
+
+const SelectedWrapper = styled.div`
+  padding: 1em;
+  border-top: 1px solid gray;
+  max-width: 60em;
+  margin-left: auto;
+  margin-right: auto;
+  box-shadow: 0 0 1em grey;
+  @media print {
+    box-shadow: none;
+    max-width: none;
+    page-break-inside: avoid;
+  }
 `;
 
 
 const TruthCategory = ({ name, options, choice, setChoice }) => {
   if (choice !== undefined && options[choice]) {
     return (
-      <Wrapper>
-        <h2 id={name}>
+      <SelectedWrapper>
+        <CategoryName id={name}>
           {name}
-          <button onClick={() => setChoice(undefined)}>Reset</button>
-        </h2>
+        </CategoryName>
         <Description>{options[choice].description}</Description>
         <QuestStarter>{options[choice].questStarter}</QuestStarter>
-      </Wrapper>
+        <ResetButton onClick={() => setChoice(undefined)}>Reset</ResetButton>
+      </SelectedWrapper>
     )
   }
   if (choice) {
     // Other
     return (
-      <Wrapper>
-        <h2 id={name}>
+      <SelectedWrapper>
+        <CategoryName id={name}>
           {name}
-          <button onClick={() => setChoice(undefined)}>Reset</button>
-        </h2>
+        </CategoryName>
+        <ResetButton onClick={() => setChoice(undefined)}>Reset</ResetButton>
         <Description>{choice}</Description>
-      </Wrapper>
+      </SelectedWrapper>
     )
   }
   return (
     <Wrapper>
-      <h2>
+      <CategoryName>
         {name}
-        <button onClick={() => setChoice(Math.floor(Math.random() * options.length))}>Random</button>
-      </h2>
+      </CategoryName>
+      <ResetButton onClick={() => setChoice(Math.floor(Math.random() * options.length))}>Random</ResetButton>
       <Choices>
         {options.map(({ description, questStarter }, index) => (
           <Choice key={index}>
