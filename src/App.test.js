9d38c24f4b6c44e3615af4986a83cd7b58281e42
Ed Brannin <edbrannin@gmail.com>
2021-06-18 00:02:26 -0400

    Fix unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Links to the Ironsworn website', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Ironsworn World Workbook/i);
   expect(linkElement).toBeInTheDocument();
 });
